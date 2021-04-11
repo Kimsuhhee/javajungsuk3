092b970ca21c6938778a82e6d4633fe178e4495d
Kimsuhhee <kimsu3819@gmail.com>
2021-04-11 16:23:24 +0900

    ch4연습문제풀이

---
+++ ch4/Exercise4_13/Exercise4_13.java
@@ -0,0 +1,20 @@
+public class Exercise4_13 {
+    public static void main(String[] args) {
+       String value = "12o34";
+       char ch = ' ';
+       boolean isNumber = true;
+
+       //반복문와 charAt(int i)를 이용해서 문자열의 문자를 하나씩 읽어서 검사
+        for(int i=0;i<value.length();i++){
+            ch = value.charAt(i);
+            if(ch-'0'>=0 && ch-'0'<=9)continue;
+            else isNumber=false;
+        }
+
+        if(isNumber){
+            System.out.println(value+"는 숫자입니다.");
+        }else{
+            System.out.println(value+"는 숫자가 아닙니다.");
+        }
+    }
+}
